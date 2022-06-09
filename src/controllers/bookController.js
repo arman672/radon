a755646fd210fd70e87e9b2198e5137c20581027
arman672 <ahmedarman672@gmail.com>
2022-06-10 01:50:50 +0530

    assignment

--- src/controllers/bookController.js
+++ src/controllers/bookController.js
@@ -1,10 +1,26 @@
-const authorModel = require("../models/authorModel")
 const bookModel= require("../models/bookModel")
+const AuthorModel= require("../models/authorModel")
+const PublisherModel = require("../models/publisherModel")
 
 const createBook= async function (req, res) {
-    let book = req.body
-    let bookCreated = await bookModel.create(book)
-    res.send({data: bookCreated})
+    let data = req.body
+    let authorID = data.author
+    let publisherID = data.publisher
+    //console.log(authorID)
+
+    let author = await AuthorModel.findOne({_id:authorID})//could have used found one
+    let publisher = await PublisherModel.findOne({_id:publisherID})
+
+    if(author === null){
+        res.send({msg: "author is not present"})  
+    }
+    else if(publisher === null){
+        res.send({msg: "publisher is not present"})
+    }
+    else{
+        let bookCreated = await bookModel.create(data)
+        res.send({bookCreated})
+    }
 }
 
 const getBooksData= async function (req, res) {
@@ -13,11 +29,23 @@ const getBooksData= async function (req, res) {
 }
 
 const getBooksWithAuthorDetails = async function (req, res) {
-    let specificBook = await bookModel.find().populate('author_id')
+    let specificBook = await bookModel.find().populate('author publisher')
     res.send({data: specificBook})
-
 }
+
+const updatePrice = async function(req,res){
+    let updatedData = await bookModel.find({rating:{$gt:3.5}}).updateMany({$inc: {"price": 10}})
+    res.send({updatedData})
+} 
+
+const updateAllfield = async function(req,res){
+    let updateFeild = await bookModel.find({rating:{$gt:0}}).updateMany({$set: {"isHardCover": true}})
+    res.send({updateFeild})
+} 
 
 module.exports.createBook= createBook
 module.exports.getBooksData= getBooksData
 module.exports.getBooksWithAuthorDetails = getBooksWithAuthorDetails
+module.exports.updatePrice = updatePrice
+
+module.exports.updateAllfield = updateAllfield
--- src/models/bookModel.js
+++ src/models/bookModel.js
@@ -1,17 +1,25 @@
+const { type } = require('express/lib/response');
 const mongoose = require('mongoose');
 const ObjectId = mongoose.Schema.Types.ObjectId
 
 const bookSchema = new mongoose.Schema( {
     name: String,
-    author_id: {
+    author: {
         type: ObjectId,
-        ref: "Author"
+        ref: "newAuthor"
     },
     price: Number,
-    ratings: Number
-
-
+    ratings: Number,
+    publisher:{
+        type: ObjectId,
+        ref:"newPublisher"
+    },
+    isHardCover: {
+        type: Boolean,
+        default: false
+    }
 }, { timestamps: true });
 
-
-module.exports = mongoose.model('LibraryBook', bookSchema)
+bookSchema.add({isHardCover:'Boolean'});
+//module.exports.bookSchema = bookSchema
+module.exports = mongoose.model('newBook', bookSchema)
