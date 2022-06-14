35b16500ff175d7fcaf474bec54f1a1d9fa64e86
arman672 <ahmedarman672@gmail.com>
2022-06-14 22:37:05 +0530

    assignment

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,4 @@
-const jwt = require("jsonwebtoken");
+//const jwt = require("jsonwebtoken");
 const userModel = require("../models/userModel");
 
 const createUser = async function (abcd, xyz) {
@@ -10,6 +10,7 @@ const createUser = async function (abcd, xyz) {
   console.log(abcd.newAtribute);
   xyz.send({ msg: savedData });
 };
+
 
 const loginUser = async function (req, res) {
   let userName = req.body.emailId;
@@ -41,22 +42,23 @@ const loginUser = async function (req, res) {
 };
 
 const getUserData = async function (req, res) {
-  let token = req.headers["x-Auth-token"];
-  if (!token) token = req.headers["x-auth-token"];
-
-  //If no token is present in the request header return error
-  if (!token) return res.send({ status: false, msg: "token must be present" });
-
-  console.log(token);
   
-  // If a token is present then decode the token with verify function
-  // verify takes two inputs:
-  // Input 1 is the token to be decoded
-  // Input 2 is the same secret with which the token was generated
-  // Check the value of the decoded token yourself
-  let decodedToken = jwt.verify(token, "functionup-radon");
-  if (!decodedToken)
-    return res.send({ status: false, msg: "token is invalid" });
+  // let token = req.headers["x-Auth-token"];
+  // if (!token) token = req.headers["x-auth-token"];
+
+  // //If no token is present in the request header return error
+  // if (!token) return res.send({ status: false, msg: "token must be present" });
+
+  // console.log(token);
+  
+  // // If a token is present then decode the token with verify function
+  // // verify takes two inputs:
+  // // Input 1 is the token to be decoded
+  // // Input 2 is the same secret with which the token was generated
+  // // Check the value of the decoded token yourself
+  // let decodedToken = jwt.verify(token, "functionup-radon");
+  // if (!decodedToken)
+  //   return res.send({ status: false, msg: "token is invalid" });
 
   let userId = req.params.userId;
   let userDetails = await userModel.findById(userId);
@@ -67,24 +69,66 @@ const getUserData = async function (req, res) {
 };
 
 const updateUser = async function (req, res) {
-// Do the same steps here:
-// Check if the token is present
-// Check if the token present is a valid token
-// Return a different error message in both these cases
+  // // Do the same steps here:
+  // // Check if the token is present
+  // // Check if the token present is a valid token
+  // // Return a different error message in both these cases
 
+  // let token = req.headers["x-Auth-token"]
+  
+  // //checking if token is present
+  // if (!token) token = req.headers["x-auth-token"];
+  // if (!token) return res.send({ status: false, msg: "token must be present" });
+
+  // //cheking if token is valid
+  // let decodedToken = jwt.verify(token, "functionup-radon");
+  // if (!decodedToken)
+  // return res.send({ status: false, msg: "token is invalid" });
+
+  //accessing the userID
+  let userId = req.params.userId;
+  console.log(userId);
+  let user = await userModel.findById(userId);
+
+  //Return an error if no user with the given id exists in the db
+  // if (!user) {
+  //   return res.send("No such user exists");
+  // }
+
+  let userData = req.body;
+  console.log(userData);
+  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData, {new: true});
+  console.log(updateUser)
+  res.send({ status: "updatedUser", data: updatedUser });
+};
+
+ const deleteUser = async function(req, res){
+  
+  // let token = req.headers["x-Auth-token"]
+  
+  // //checking if token is present
+  // if (!token) token = req.headers["x-auth-token"];
+  // if (!token) return res.send({ status: false, msg: "token must be present" });
+
+  // //cheking if token is valid
+  // let decodedToken = jwt.verify(token, "functionup-radon");
+  // if (!decodedToken)
+  // return res.send({ status: false, msg: "token is invalid" });
+
+  //accessing the userID
   let userId = req.params.userId;
   let user = await userModel.findById(userId);
+
   //Return an error if no user with the given id exists in the db
   if (!user) {
     return res.send("No such user exists");
   }
 
-  let userData = req.body;
-  let updatedUser = await userModel.findOneAndUpdate({ _id: userId }, userData);
-  res.send({ status: updatedUser, data: updatedUser });
-};
-
+  let deleteUser = await userModel.findOneAndUpdate({ _id: userId }, {isDeleted: true}, {new:true});
+  res.send({ status: "deletedUser", data: deleteUser });
+ }
 module.exports.createUser = createUser;
 module.exports.getUserData = getUserData;
 module.exports.updateUser = updateUser;
 module.exports.loginUser = loginUser;
+module.exports.deleteUser = deleteUser;
--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-mongoose.connect("mongodb+srv://users-open-to-all:hiPassword123@cluster0.uh35t.mongodb.net/skDB?retryWrites=true&w=majority", {
+mongoose.connect("mongodb+srv://Arman:W0ZPcEp2jiZXKgid@cluster0.ilfh6.mongodb.net/auth-1", {
     useNewUrlParser: true
 })
 .then( () => console.log("MongoDb is connected"))
