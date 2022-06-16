94765d4dd4448e9f2a363602339c1cd3d984e157
arman672 <ahmedarman672@gmail.com>
2022-06-16 22:50:35 +0530

    assignment

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -1,38 +1,42 @@
 const jwt = require("jsonwebtoken");
 const authenticate = function(req, res, next){
+   try{
     let token = req.headers["x-Auth-token"];
-     if (!token) token = req.headers["x-auth-token"];
-
-     //If no token is present in the request header return error
-    if (!token) return res.send({ status: false, msg: "token must be present" });
-
+    if (!token) token = req.headers["x-auth-token"];
+    if (!token) return res.status(400).send({ status: false, msg: "token must be present" });
     console.log(token);
-  
-    // If a token is present then decode the token with verify function
-    // verify takes two inputs:
-    // Input 1 is the token to be decoded
-    // Input 2 is the same secret with which the token was generated
-    // Check the value of the decoded token yourself
     let decodedToken = jwt.verify(token, "functionup-radon");
     if (!decodedToken)
-    return res.send({ status: false, msg: "token is invalid" });
+    return res.send({ status: false, msg: "token" });
     next()
+   }
+   catch(err){
+     res.status(500).send({ msg: "Error", error: err.message })
+   }
 }
 
 const authorise = function(req, res, next) {
+   try{
     let token = req.headers["x-auth-token"]
     let decodedToken = jwt.verify(token, 'functionup-radon')
 
-    if(!decodedToken) return res.send({status: false, msg:"token is not valid"})
+    if(!decodedToken) return res.status(400).send({status: false, msg:"token is not valid"})
     
     //userId for which the request is made. In this case message to be posted.
     let userToBeModified = req.params.userId
     //userId for the logged-in user
     let userLoggedIn = decodedToken.userId
 
+    //console.log(1/a)
+
     //userId comparision to check if the logged-in user is requesting for their own data
-    if(userToBeModified != userLoggedIn) return res.send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
+    if(userToBeModified != userLoggedIn) return res.status(403).send({status: false, msg: 'User logged is not allowed to modify the requested users data'})
+    
     next()
+   }
+    catch(err){
+        res.status(500).send({ msg: "Error", error: err.message })
+   }
 }
 
 module.exports.authenticate = authenticate
